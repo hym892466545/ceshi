2f915e45cb144085345668c7bc09b6b9157c2545
hym892466545 <892466545@qq.com>
2017-08-24 22:32:17 +0800

    在家测试

---
+++ test.js
@@ -0,0 +1,23 @@
+var obj = {
+	init : function(){},
+	showDialog : function(title, message){},
+	showModelDialog : function(title, message){}
+}
+
+var obj2 = {
+	defaultOption : {width:xx,height:xxx,imgs:[{src:"",href:""}]},
+	init : function(){},
+	register : function(){},
+	move : function(){},
+	fade : function(){},
+	slide : function(){},
+}
+
+function xxx(option){
+	this.xx = xx;
+	this.yy = xxx;
+}
+xxx.prototype = {
+	init : function(){},
+	move : function(){}
+}
